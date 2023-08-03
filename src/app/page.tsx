9eeb381e428f41dfd18dd9cdcf4a884091fcd24f
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-08-03 15:20:13 -0300

    🌙 feat: Dark Mode no Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-slate-900 p-8 text-slate-100">
-      <h1 className="flex items-center gap-3 text-3xl font-bold before:flex before:h-8 before:w-0.5 before:bg-sky-500 sm:text-5xl lg:text-6xl">
-        Hello Tailwind
-      </h1>
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
-      <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium transition-colors enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60">
-        Sign in
-      </button>
+    <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+      <div className="max-w-2xl">
+        <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">
+          Rapidly build modern websites without ever leaving your HTML.
+        </h1>
+        <p className="mt-4 dark:text-slate-400">
+          A utility-first CSS framework packed with classes like flex, pt-4,
+          text-center and rotate-90 that can be composed to build any design,
+          directly in your markup.
+        </p>
+        <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium text-white transition-colors enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-sky-400 dark:text-sky-950">
+          Get started
+        </button>
+      </div>
     </div>
   );
 }
