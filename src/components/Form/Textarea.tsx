4615b2562b9a7c74ce6c832ec9152dc6aecd6d5f
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-08-18 20:25:11 -0300

    ✨ feat: Criando efeitos de foco

--- src/components/Form/Textarea.tsx
+++ src/components/Form/Textarea.tsx
@@ -5,7 +5,7 @@ interface ITextarea extends ComponentProps<"textarea"> {}
 export function Textarea({ ...props }: ITextarea) {
   return (
     <textarea
-      className="min-h-[7.5rem] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="min-h-[7.5rem] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100"
       {...props}
     />
   );
